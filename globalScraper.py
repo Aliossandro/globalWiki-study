63f12d6e56fb9b082c55885b8cdc9439a258385b
Aliossandro <alessandro.piscopo@soton.ac.uk>
2018-08-06 14:44:28 +0100

    global scraper fixed - starts where left last time

--- globalScraper.py
+++ globalScraper.py
@@ -2,6 +2,8 @@ import urllib
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
+import http
+import time
 
 class userGetter:
 
@@ -15,39 +17,67 @@ class userGetter:
 
 
     def pageNavigator(self, startLink):
-        page = urllib.request.urlopen(self.baseUrl + startLink)
+        try:
+            page = urllib.request.urlopen(self.baseUrl + startLink)
+            # parse the html using beautiful soup and store in variable `soup`
+            soup = BeautifulSoup(page, 'html.parser')
+            nextLink = soup.find("a", class_="mw-nextlink")
 
-        # parse the html using beautiful soup and store in variable `soup`
-        soup = BeautifulSoup(page, 'html.parser')
-        nextLink = soup.find("a", class_="mw-nextlink")
-
-        if nextLink:
-            newLink = nextLink.get('href')
-            print(newLink)
-            try:
-                self.globalUserExtractor(newLink)
-            except RecursionError:
+            if nextLink:
+                newLink = nextLink.get('href')
                 print(newLink)
-                self.fileWriter()
+                try:
+                    self.globalUserExtractor(newLink)
+                except RecursionError:
+                    print(newLink)
+                    self.fileWriter()
+        except (http.client.IncompleteRead) as e:
+            print(e)
+            page = e.partial
+            print(page)
+
+            time.sleep(360)
+            page = urllib.request.urlopen(self.baseUrl + startLink)
+            # parse the html using beautiful soup and store in variable `soup`
+            soup = BeautifulSoup(page, 'html.parser')
+            nextLink = soup.find("a", class_="mw-nextlink")
+
+            if nextLink:
+                newLink = nextLink.get('href')
+                print(newLink)
+                try:
+                    self.globalUserExtractor(newLink)
+                except RecursionError:
+                    print(newLink)
+                    self.fileWriter()
+
+
 
 
     def globalUserExtractor(self, startLink):
+
+        try:
         # parse the html using beautiful soup and store in variable `soup`
-        page = urllib.request.urlopen(self.baseUrl + startLink)
-        soup = BeautifulSoup(page, 'html.parser')
-        name_box = soup.find('ul')
+            page = urllib.request.urlopen(self.baseUrl + startLink)
+            soup = BeautifulSoup(page, 'html.parser')
+            name_box = soup.find('ul')
 
-        for li in name_box:
-            item = li.find('a')
-            try:
-                self.accountList.append(item.get('href').replace('/wiki/Special:CentralAuth/', ''))
-                if len(self.accountList) >= 500000:
-                    self.fileWriter()
-                    self.accountList = []
-            except AttributeError:
-                print(item)
+            for li in name_box:
+                item = li.find('a')
+                try:
+                    self.accountList.append(item.get('href').replace('/wiki/Special:CentralAuth/', ''))
+                    if len(self.accountList) >= 500000:
+                        self.fileWriter()
+                        self.accountList = []
+                except AttributeError:
+                    print(item)
 
-        self.pageNavigator(startLink)
+            self.pageNavigator(startLink)
+
+        except (http.client.IncompleteRead) as e:
+            print(e)
+            page = e.partial
+            print(page)
 
     def fileWriter(self):
         with open('globalUserList.txt', 'a+') as f:
@@ -60,7 +90,9 @@ class userGetter:
 
 def main():
     # other_path = '/Users/alessandro/Documents/PhD/WD_ontology'
-    startLink = '/w/index.php?title=Special:GlobalUsers&offset=&limit=5000'
+    # startLink = '/w/index.php?title=Special:GlobalUsers&offset=&limit=5000'
+    startLink = '/w/index.php?title=Special:GlobalUsers&offset=Ag2solo&limit=5000'
+
     userGetter(startLink)
 
 
